df85824ee4fa846e8690e9b02a5abc2ebfc7a573
Sarvadhi Solution <sarvadhisolution@Sarvadhis-MacBook-Pro.local>
2026-01-12 17:56:26 +0530

    nodejs

---
+++ count.js
@@ -0,0 +1,19 @@
+const x = 1;
+const y = 2;
+const z = 3;
+
+console.count(
+  'The value of x is ' + x + ' and has been checked .. how many times?'
+);
+
+console.count(
+  'The value of x is ' + x + ' and has been checked .. how many times?'
+);
+
+console.count(
+  'The value of y is ' + y + ' and has been checked .. how many times?'
+);
+//output 
+/*The value of x is 1 and has been checked .. how many times?: 1
+The value of x is 1 and has been checked .. how many times?: 2
+The value of y is 2 and has been checked .. how many times?: 1*/
